0a8ce5503d466d9235fc264d62dfb561cbef7c66
Ana Paula <msorigamiasd@gmail.com>
2026-04-08 03:24:42 -0500

    CRUD completo de incidentes funcional

--- incidentes/views.py
+++ incidentes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Incidente
 from .forms import IncidenteForm
 
@@ -21,3 +21,22 @@ def registrar_incidente(request):
 def lista_incidentes(request):
     incidentes = Incidente.objects.all().order_by('-fecha')
     return render(request, 'lista_incidentes.html', {'incidentes': incidentes})
+
+
+def editar_incidente(request, id):
+    incidente = get_object_or_404(Incidente, id=id)
+
+    if request.method == 'POST':
+        form = IncidenteForm(request.POST, instance=incidente)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_incidentes')
+    else:
+        form = IncidenteForm(instance=incidente)
+
+    return render(request, 'registrar_incidente.html', {'form': form})
+
+def eliminar_incidente(request, id):
+    incidente = get_object_or_404(Incidente, id=id)
+    incidente.delete()
+    return redirect('lista_incidentes')   
--- somasafe/urls.py
+++ somasafe/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('registrar/', views.registrar_incidente, name='registrar_incidente'),
     path('incidentes/', views.lista_incidentes, name='lista_incidentes'),
+    path('editar/<int:id>/', views.editar_incidente, name='editar_incidente'),
+    path('eliminar/<int:id>/', views.eliminar_incidente, name='eliminar_incidente'),
 ]
